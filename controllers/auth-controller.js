83a3658ad20ee71ec3e48e2bbf5ce0ebad7942c5
katesyvash795 <126354194+katesyvash795@users.noreply.github.com>
2024-01-03 20:07:15 +0200

    commit 1

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import Jimp from 'jimp';
+import { nanoid } from "nanoid";
 import {generateAvatarUrl} from '../helpers/index.js';
 
 import User from "../models/User.js";
@@ -20,7 +21,7 @@ const signup = async (req, res) => {
 	}
 
 	const hashPassword = await bcrypt.hash(password, 10);
-
+    const verificationToken = nanoid();
 	const avatar = generateAvatarUrl(email, {
 		defaultImage: 'monsterid',
 	});
@@ -29,14 +30,65 @@ const signup = async (req, res) => {
 		...req.body,
 		password: hashPassword,
 		avatarUrl: avatar,
+        verificationToken,
 	});
 
-	res.status(201).json({
-		email: newUser.email,
-		subscription: newUser.subscription,
-	});
+    const verifyEmail = {
+        to: email,
+        subject: "Сonfirm your registration",
+        html: `<a target="_blank" href="${BASE_URL}/users/verify/${verificationToken}">Click to confirm your registration</a>`,
+      };
+    
+      await sendEmail(verifyEmail);
+    
+  res.status(201).json({
+    user: { email: result.email, subscription: result.subscription },
+  });
 };
   
+const verify = async (req, res) => {
+    const { verificationToken } = req.params;
+  
+    const user = await User.findOne({ verificationToken });
+  
+    if (!user) {
+      throw HttpError(404, "User not found");
+    }
+  
+    await User.findByIdAndUpdate(user._id, {
+      verify: true,
+      verificationToken: "",
+    });
+  
+    res.json({
+      message: "Verification successful",
+    });
+  };
+
+  const resendVerify = async (req, res) => {
+    const { email } = req.body;
+  
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw HttpError(404, "User not found");
+    }
+  
+    if (user.verify) {
+      throw HttpError(400, "Verification has already been passed");
+    }
+  
+    const verifyEmail = {
+      to: email,
+      subject: "Сonfirm your registration",
+      html: `<a target="_blank" href="${BASE_URL}/users/verify/${user.verificationToken}">Click to confirm your registration</a>`,
+    };
+  
+    await sendEmail(verifyEmail);
+  
+    res.json({
+      message: "Verification email sent",
+    });
+  };
 
 const signin = async (req, res) => {
     const { email, password } = req.body;
@@ -44,6 +96,10 @@ const signin = async (req, res) => {
     if (!user) {
         throw HttpError(401, "Email or password invalid");
     }
+    if (!user.verify) {
+        throw HttpError(401, "User not found");
+      }
+
     const passwordCompare = await bcrypt.compare(password, user.password);
     if (!passwordCompare) {
         throw HttpError(401, "Email or password invalid");
@@ -79,6 +135,23 @@ const signout = async(req, res)=> {
     })
 }
 
+const updateSubscription = async (req, res) => {
+    const { _id: owner } = req.user;
+    const { subscription } = req.body;
+  
+    const result = await User.findByIdAndUpdate(
+      owner,
+      { subscription },
+      { new: true }
+    );
+  
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+  
+    res.json(result);
+  };
+
 const updateAvatar = async (req, res) => {
 	const { _id } = req.user;
 
@@ -101,9 +174,12 @@ const updateAvatar = async (req, res) => {
 };
 
 export default {
+    verify: ctrlWrapper(verify),
+  resendVerify: ctrlWrapper(resendVerify),
     signup: ctrlWrapper(signup),
     signin: ctrlWrapper(signin),
     getCurrent: ctrlWrapper(getCurrent),
     signout: ctrlWrapper(signout),
-    updateAvatar:ctrlWrapper(updateAvatar)
+    updateAvatar:ctrlWrapper(updateAvatar),
+    updateSubscription: ctrlWrapper(updateSubscription),
 }
